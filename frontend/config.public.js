b0f419976f30d354bd9da37bc49752f0b5fbaf8c
siddharthcode-24 <siddharthbajaj24@gmail.com>
2025-11-02 07:31:19 +0530

    Update Supabase URL in config.public.js

--- frontend/config.public.js
+++ frontend/config.public.js
@@ -1,3 +1,3 @@
 // ⚠️ Add this file to .gitignore before committing to GitHub
-const SUPABASE_URL = "https://wtqeuuospotwrdeunjal.supabase.co";
+const SUPABASE_URL = "https://xyz.supabase.co";
 const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Ind0cWV1dW9zcG90d3JkZXVuamFsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjE4OTkxOTksImV4cCI6MjA3NzQ3NTE5OX0.3N4fe4vtytqeThBxC2hrKwxejh-_iI5gVNjPdy6ruvI";
